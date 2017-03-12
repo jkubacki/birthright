2617ff5673c2ac6b856f7732ffa10231801ce741
Jakub Kubacki <kubacki.jk@gmail.com>
2017-03-12 22:03:32 +0100

    Add sample seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+UnitType.find_or_create_by(
+  name: "Infantry",
+  discipline: 6,
+  discipline_close: 6,
+  armor: 3,
+  defense: 5,
+  hp: 9,
+  fighting: 3,
+  shooting: 0,
+  fighting_damage: 4,
+  shooting_damage: 0,
+  range: 0,
+  speed: 4
+)
+
+UnitType.find_or_create_by(
+  name: "Archers",
+  discipline: 6,
+  discipline_close: 9,
+  armor: 2,
+  defense: 7,
+  hp: 6,
+  fighting: 2,
+  shooting: 3,
+  fighting_damage: 2,
+  shooting_damage: 4,
+  range: 10,
+  speed: 4
+)
