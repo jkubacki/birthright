bfb2d2797d17cce6ad2204402059f8e978fc92d8
Jakub Kubacki <kubacki.jk@gmail.com>
2017-03-12 17:13:02 +0100

    Add onclick to panel

--- app/javascript/packs/components/panel.js
+++ app/javascript/packs/components/panel.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 export default class Panel extends React.Component {
 
+  _toggle() {
+    console.log("clicked");
+  }
+
   render() {
     return (
-      <div className="Panel">
+      <div className="Panel" onClick={this._toggle}>
         Panel {this.props.clicked} clicked
       </div>
     );
