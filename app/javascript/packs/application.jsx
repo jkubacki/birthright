e9a9b9a2ed324aad8896bdf693e0b7b74e953e6c
Jakub Kubacki <kubacki.jk@gmail.com>
2017-03-12 13:55:17 +0100

    Add react as a main app

--- app/javascript/packs/application.jsx
+++ app/javascript/packs/application.jsx
@@ -7,4 +7,24 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Hello = props => (
+  <div>Hello {props.name}!</div>
+)
+
+Hello.defaultProps = {
+  name: 'David'
+}
+
+Hello.propTypes = {
+  name: React.PropTypes.string
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Hello name="React" />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
